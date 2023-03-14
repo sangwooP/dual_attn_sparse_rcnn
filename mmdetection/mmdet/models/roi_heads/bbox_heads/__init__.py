693def3d68cb667ad0323fd72896edf573dc5134
sang <swtkdn@gmail.com>
2023-03-14 16:52:53 +0900

    Initial commit

---
+++ mmdetection/mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .bbox_head import BBoxHead
+from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
+                               Shared4Conv1FCBBoxHead)
+from .dii_head import DIIHead
+from .dual_attn_head import DualAttnHead
+from .double_bbox_head import DoubleConvFCBBoxHead
+from .drtr_head import DRTRHead
+from .sabl_head import SABLHead
+from .scnet_bbox_head import SCNetBBoxHead
+
+__all__ = [
+    'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
+    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'DRTRHead', 'SABLHead', 'DIIHead',
+    'DualAttnHead',
+    'SCNetBBoxHead'
+]
